72fd0fabc04b5e6470ac3d60d704db571d6651db
lqtungntn <lqtungntn@gmail.com>
2022-09-19 22:58:47 +0700

    update zombie animation
    - update zombie dead
    - update zombie appear

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ pygame.display.set_caption('HIT ZOMBIE!!!')
 FPS = 60
 MAP = pygame.transform.scale(pygame.image.load(
     os.path.join('Assets', 'Night.png')), (WIDTH, HEIGHT))
-TOMBSTONE = pygame.transform.scale(pygame.image.load(
-    os.path.join('Assets', 'tombstone.png')), (100, 100))
 MOUSE = pygame.transform.scale(pygame.image.load(
     os.path.join('Assets', 'Mouse', 'Hammer.png')), (40, 75))
 HIT_IMAGE = pygame.transform.scale(pygame.image.load(
@@ -31,7 +29,32 @@ frame_count = 0
 mouse_delay_count = 0
 eventx, eventy = 0, 0
 onHit = False
+DeadList = [0 for i in range(0, 45)]
+class Zombie(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.appear = []
+        self.appear.append(pygame.image.load('Assets\\Zombie\\Zombie_1.png'))
+        self.appear.append(pygame.image.load('Assets\\Zombie\\Zombie_2.png'))
+        self.appear.append(pygame.image.load('Assets\\Zombie\\Zombie_3.png'))
+        self.appear.append(pygame.image.load('Assets\\Zombie\\Zombie_4.png'))
+        self.appear.append(pygame.image.load('Assets\\Zombie\\Zombie_5.png'))
+        self.appear.append(pygame.image.load('Assets\\Zombie\\Zombie_6.png'))
+        self.appear.append(pygame.image.load('Assets\\Zombie\\Zombie_7.png'))
+        self.appear.append(pygame.image.load('Assets\\Zombie\\Zombie_8.png'))
+        self.dead = []
+        self.dead.append(pygame.image.load('Assets\\Zombie\\Zombie_Dead_1.png'))
+        self.dead.append(pygame.image.load('Assets\\Zombie\\Zombie_Dead_2.png'))
+        self.dead.append(pygame.image.load('Assets\\Zombie\\Zombie_Dead_3.png'))
+        self.dead.append(pygame.image.load('Assets\\Zombie\\Zombie_Dead_4.png'))
+        self.dead.append(pygame.image.load('Assets\\Zombie\\Zombie_Dead_5.png'))
+        self.dead.append(pygame.image.load('Assets\\Zombie\\Zombie_Dead_6.png'))
 
+    def update_appear(self, frame):
+        return self.appear[frame]
+
+    def update_dead(self, frame):
+        return self.dead[frame]
 def draw_mouse_cursor():
     global mouse_delay_count
     global MOUSE
@@ -51,13 +74,19 @@ def draw_pow(pos):
     WIN.blit(hit_image_copy, pos)
 
 def draw_tomestone(pos):
-    tombstone_copy = TOMBSTONE.copy()
-    # pos_change = (pos[0], pos[1] + 100 / (frame_count%60 + 1))
-    # WIN.blit(TOMBSTONE, pos_change, (0,0,100 ,100 / (frame_count%60 + 1)))
-    pic_pos = WIN.blit(tombstone_copy, pos)
+    tombstone_copy = Zombie()
+    pic_pos = WIN.blit(tombstone_copy.update_appear(7), pos)
     if pic_pos.collidepoint(eventx, eventy):
         return True
     return False
+
+def draw_appear_zombie(pos, frame):
+    zombie_copy = Zombie()
+    WIN.blit(zombie_copy.update_appear(int(frame)), pos)
+
+def draw_dead_zombie(pos, frame):
+    zombie_copy = Zombie()
+    WIN.blit(zombie_copy.update_dead(int(frame)), pos)
 
 def draw_background():
     global hit, miss, onHit
@@ -74,17 +103,30 @@ def draw_background():
     hit_image = False
 
     for idx in range(len(TOMBSTONE_LIST)):
-        if TOMBSTONE_LIST[idx][1] != 0 and TOMBSTONE_LIST[idx][2]:
-            check_hit = draw_tomestone([BOARD[0] + (TOMBSTONE_LIST[idx][0] % 9)*75, BOARD[1] + (TOMBSTONE_LIST[idx][0] // 9)*75])
-            if check_hit:
-                TOMBSTONE_LIST[idx][2] = False
-                hit_image = True
-                POW_LIST.append([[eventx, eventy], 60])
+        if TOMBSTONE_LIST[idx][1] <= 60 and TOMBSTONE_LIST[idx][1] > 0:
+            TOMBSTONE_LIST[idx][1] -= 1
+            if DeadList[idx] > 0:
+                draw_dead_zombie([BOARD[0] + (TOMBSTONE_LIST[idx][0] % 9)*75, BOARD[1] + (TOMBSTONE_LIST[idx][0] // 9)*75], (6 / FPS) * (FPS - DeadList[idx]))
+                DeadList[idx] -= 1
+            elif DeadList[idx] == 0:
+                draw_appear_zombie([BOARD[0] + (TOMBSTONE_LIST[idx][0] % 9) * 75, BOARD[1] + (TOMBSTONE_LIST[idx][0] // 9) * 75], (8 / FPS) * (TOMBSTONE_LIST[idx][1] - 1))
+        elif TOMBSTONE_LIST[idx][1] != 0 and TOMBSTONE_LIST[idx][2]:
+            if TOMBSTONE_LIST[idx][1] <= 300:
+                check_hit = draw_tomestone([BOARD[0] + (TOMBSTONE_LIST[idx][0] % 9)*75, BOARD[1] + (TOMBSTONE_LIST[idx][0] // 9)*75])
+                if check_hit:
+                    TOMBSTONE_LIST[idx][2] = False
+                    hit_image = True
+                    POW_LIST.append([[eventx, eventy], FPS])
+                    DeadList[idx] = FPS
+                    TOMBSTONE_LIST[idx][1] = FPS + 1
+            else:
+                draw_appear_zombie([BOARD[0] + (TOMBSTONE_LIST[idx][0] % 9)*75, BOARD[1] + (TOMBSTONE_LIST[idx][0] // 9)*75], (8 / FPS) * (360 - TOMBSTONE_LIST[idx][1]))
             TOMBSTONE_LIST[idx][1] -= 1
         elif TOMBSTONE_LIST[idx][1] <= 0:
             DEL_LIST.append(idx)
         elif TOMBSTONE_LIST[idx][2] == False:
             TOMBSTONE_LIST[idx][1] -= 1
+
 
     for idx in DEL_LIST:
         CHECK[TOMBSTONE_LIST[idx][0]] = False
@@ -141,7 +183,7 @@ def main():
             while ((CHECK[RANDOM]) and (CHECK.count(False) != 0)):
                 RANDOM = random.randint(0, 44)
             CHECK[RANDOM] = True
-            TOMBSTONE_LIST.append([RANDOM, 300, True])
+            TOMBSTONE_LIST.append([RANDOM, 360, True])
         elif frame_count == FPS:
             frame_count = 0
         draw_background()
